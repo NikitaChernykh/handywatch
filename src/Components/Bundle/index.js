3669f9dbc6cd3712a19700bbe894b497c718bb16
NikitaChernykh <chernykhmain@gmail.com>
2019-06-01 22:13:18 -0400

    GMS-0001: Initial commit

---
+++ src/Components/Bundle/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Bundle extends Component {
+    render() {
+        return (
+            <div>
+                bundle ready
+            </div>
+        );
+    }
+}
+
+export default Bundle;

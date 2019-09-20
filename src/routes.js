38f45179e7489c85d86d592d1c4efc4da9a21926
nikitachernyk <nikita.chernykh@borrowell.com>
2019-09-20 17:02:04 -0400

    updated bundle link

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,6 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Faq from "./Components/Faq";
 import ClockfaceDetails from "./Components/ClockfaceDetails";
-import Bundles from "./Components/Bundles";
 
 class Routes extends React.Component {
   componentDidUpdate() {
@@ -33,7 +32,10 @@ class Routes extends React.Component {
             component={Home}
             path="/fitbitsettings"
           />
-          <Route {...this.props} exact component={Bundles} path="/bundles" />
+          <Route exact path="/bundles" render={() => {
+            window.location.href = "https://k-pay.io/code/?s=21128EF3-D81C-C4A4-6B21-82C482CD1C6E&dsu=158823&p=1721-1190-1023-1064-1427-2318";
+            return null;
+          }} />
           <Route {...this.props} exact component={ClockfaceDetails} path="/details/:id" />
           <Route {...this.props} exact component={Home} path="/" />
         </Switch>

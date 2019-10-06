0d3de61010313f554f12dcf5a3be17cb30153416
NikitaChernykh <chernykhmain@gmail.com>
2019-10-06 19:01:50 -0400

    Updated slider speed

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -50,7 +50,7 @@ class Banner extends Component {
             nextArrow: <SampleNextArrow />,
             prevArrow: <SamplePrevArrow />,
             autoplay: true,
-            autoplaySpeed: 3000,
+            autoplaySpeed: 5000,
             pauseOnHover: true,
             arrows: isMobile? false : true,
             dots: isMobile? true : false,

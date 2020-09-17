6bb1ea2452c42e4f34eeaea7fe026660bace1e79
NikitaChernykh <chernykhmain@gmail.com>
2020-09-17 19:30:58 -0400

    added links

--- src/app/components/hw-button/hw-button.component.ts
+++ src/app/components/hw-button/hw-button.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HwButtonComponent implements OnInit {
   @Input() buttonText;
   @Input() buttonType = 'dark';
+  @Input() link = 'https://gallery.fitbit.com/developer/12152438-2164-45e6-8de8-e408fb54f9a6';
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +17,10 @@ export class HwButtonComponent implements OnInit {
 
   setButtonType() {
 
+  }
+
+  onClick() {
+    document.location.href = this.link;
   }
 
 }
--- src/app/components/hw-link/hw-link.component.ts
+++ src/app/components/hw-link/hw-link.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HwLinkComponent implements OnInit {
   @Input() linkText;
+  @Input() url;
   constructor() { }
 
   ngOnInit() {

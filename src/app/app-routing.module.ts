7901cba12c3b81200a7e816670aae94973f44349
NikitaChernykh <chernykhmain@gmail.com>
2020-09-25 17:41:57 -0400

    small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrivacyComponent } from './privacy-page/privacy/privacy.component';
-import { AppComponent } from './app.component';
 import { LandingComponent } from './landing-page/landing.component';
 
 
 const routes: Routes = [
-  { path: 'privacy', component: PrivacyComponent },
   { path: '', component: LandingComponent },
+  { path: 'privacy', component: PrivacyComponent },
 ];
 
 @NgModule({

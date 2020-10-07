f005ae41f9b09cc8c3d429cad32aa263ff832c6f
NikitaChernykh <chernykhmain@gmail.com>
2020-10-06 22:10:54 -0400

    added fitbitsettings route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { LandingComponent } from './landing-page/landing.component';
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'bundles', component: LandingComponent },
+  { path: 'fitbitsettings', component: LandingComponent },
   { path: 'privacy', component: PrivacyComponent },
 ];
 

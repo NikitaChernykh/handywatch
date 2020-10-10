28d5db45253caab1be669d4f8b8db9e2100a5c50
NikitaChernykh <chernykhmain@gmail.com>
2020-10-10 13:44:13 -0400

    performace optimiztion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { PrivacyComponent } from './privacy-page/privacy/privacy.component';
 import { LandingComponent } from './landing-page/landing.component';
 
@@ -12,7 +12,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      preloadingStrategy: PreloadAllModules
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
